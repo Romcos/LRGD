aae7cbe13b6b27bdb35033d2f1b7aae4286977b3
cosson <cosson@mit.edu>
2022-10-06 15:15:45 +0200

    many improvements

--- .ipynb_checkpoints/visuals-checkpoint.py
+++ .ipynb_checkpoints/visuals-checkpoint.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
 
-def show_trajs(Z1, Z2 = None, f= lambda x:0, title = "a plot", num = 5, savename = "", colorbar = True):
+def show_trajs(Z1, Z2 = None, f= lambda x:0, title = "a plot", num = 5, savename = None, colorbar = True):
     """
     Displays one (or two if Z2 is not None) trajectories in 2D [-0.5,2]x[-0.5,2]
     
@@ -48,8 +49,81 @@ def show_trajs(Z1, Z2 = None, f= lambda x:0, title = "a plot", num = 5, savename
     plt.title(title)
     plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
 
-    if (len(savename) > 2):
+    if not(savename is None):
         plt.savefig(savename, bbox_inches='tight')
 
     plt.show()
+
     
+def plot_convergence(Zs, Cs, f, labels=["","",""], verticals = None,colors = None, savename=None,xmax=None,ymin=None):
+    """
+    Displays the decay of function value f for multiple decents
+    colors should contain at least as many entries as Zs.
+    """
+    fig, ax = plt.subplots(1, 1, figsize = (5,5))
+    
+    if (colors is None):
+        colors = pl.cm.jet(np.linspace(0,1,len(Zs)))
+    
+    if (verticals is None):
+        verticals= [False for _ in range(len(Zs))]
+    
+    for k in range(len(Zs)): 
+        ax.plot(np.cumsum(Cs[k]),[f(Zs[k][i]) for i in range(len(Zs[k]))], label = labels[k], color=colors[k])
+        if verticals[k]:
+            for i,c in enumerate(Cs[k]):
+                if c==np.max(Cs[k]):
+                    ax.axvline(x = np.sum(Cs[k][:i]), color = colors[k], linestyle = "--", linewidth = 0.5)
+                
+    ax.set_yscale("log")
+    ax.set_ylabel(r"$f(\theta)$ (log scale)")
+    ax.set_xlabel("oracle calls")
+    if not(xmax is None):
+        ax.set_xlim(left=0,right=xmax)
+    else:
+        ax.set_xlim(left=0)
+        
+    if (ymin is None):
+        ymin = np.min([f(Zs[i][-1]) for i in range(len(Zs))])
+    else:
+        ax.set_ylim(bottom=ymin)
+    ax.legend()
+    if not(savename is None):
+        fig.savefig(savename, bbox_inches='tight')
+    plt.show()
+    
+def plot_convergence2(Zs, Cs, f,df, labels=["","",""], verticals = None,colors = None, savename=None,xmax=None,ymin=None):
+    """
+    Displays the decay of function value f for multiple decents
+    colors should contain at least as many entries as Zs.
+    """
+    fig, ax = plt.subplots(1, 1, figsize = (5,5))
+    
+    if (colors is None):
+        colors = pl.cm.jet(np.linspace(0,1,len(Zs)))
+    
+    if (verticals is None):
+        verticals = [False for _ in range(len(Zs))]
+    
+    for k in range(len(Zs)): 
+        ax.plot(np.cumsum(Cs[k]),[np.linalg.norm(df(Zs[k][i])) for i in range(len(Zs[k]))], label = labels[k], color=colors[k])
+        if verticals[k]:
+            for i,c in enumerate(Cs[k]):
+                if c==np.max(Cs[k]):
+                    ax.axvline(x = np.sum(Cs[k][:i]), color = colors[k], linestyle = "--", linewidth = 0.5)
+                
+    ax.set_yscale("log")
+    ax.set_ylabel(r"$|| ~\nabla f(\theta) ~||$ (log scale)")
+    ax.set_xlabel("oracle calls")
+    if not(xmax is None):
+        ax.set_xlim(left=0,right=xmax)
+    else:
+        ax.set_xlim(left=0)
+        
+    if (ymin is not None):
+        ax.set_ylim(bottom=ymin)
+        
+    ax.legend()
+    if not(savename is None):
+        fig.savefig(savename, bbox_inches='tight')
+    plt.show()
